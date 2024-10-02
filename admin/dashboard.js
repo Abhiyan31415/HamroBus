a0f409f232bbaa8059ad2aa0acb080086321bf9a
Abhiyan khanal <abhiyankhanal7@gmail.com>
2024-10-02 21:19:15 +0545

    basic frame for admin side completed

---
+++ admin/dashboard.js
@@ -0,0 +1,13 @@
+const hamburger = document.getElementById('hamburger');
+const drawer = document.getElementById('drawer');
+const drawerClose = document.getElementById('drawer-close');
+
+// Open drawer when hamburger is clicked
+hamburger.addEventListener('click', () => {
+  drawer.classList.add('open');
+});
+
+// Close drawer when close button is clicked
+drawerClose.addEventListener('click', () => {
+  drawer.classList.remove('open');
+});
